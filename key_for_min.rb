2e1e9f772eacdb5de5f66df62d596cde0054b806
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-24 23:31:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |key, value|
+    min = 0
+    for i in key
+      if i < min
+        min = i
+      end
+      i += 1 
+    end
+      
 end
+
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+key_for_min_value(ikea)
+# => :chair
